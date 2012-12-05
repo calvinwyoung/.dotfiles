34b7a258ca9b9f644c3e816a6152b6f4553ef229
Calvin Young <calvinwyoung@gmail.com>
2012-12-05 13:25:00 -0800

    Updated oh-my-zsh. Emacs file completions now omits files in file-completion-extensions list

--- emacs.d/config/settings.el
+++ emacs.d/config/settings.el
@@ -95,3 +95,16 @@
 
 ;; Stop emacs from prompting us before killing buffers in daemon mode
 (remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
+
+;; Files with extensions in the completion-ignored-extensions list will be
+;; omitted from the file completions list.
+(defadvice completion--file-name-table (after
+                                        ignoring-backups-f-n-completion
+                                        activate)
+  "Filter out results when the have completion-ignored-extensions"
+  (let ((res ad-return-value))
+    (if (and (listp res)
+             (stringp (car res))
+             (cdr res))                 ; length > 1, don't ignore sole match
+        (setq ad-return-value
+              (completion-pcm--filename-try-filter res)))))
