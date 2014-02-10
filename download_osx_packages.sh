e43316ca25bfc3840ce9cf2780a4616ce48a8943
Calvin Young <calvinwyoung@gmail.com>
2014-02-09 16:35:01 -0800

    Refactors download_osx_packages command to open up web pages for packges

--- download_osx_packages.sh
+++ download_osx_packages.sh
@@ -1,29 +1,38 @@
 #!/usr/bin/env bash
 
-URLS=(
-    # Hotkeys and window management
-    "https://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-9.2.0.dmg"
-    "https://pqrs.org/macosx/keyremap4macbook/files/PCKeyboardHack-10.5.0.dmg"
-    "http://slate.ninjamonkeysoftware.com/Slate.dmg"
+# sudo port install bash-completion
+# sudo port install git-core +bash_completion
+# sudo port install emacs-app
 
-    # Dev tools
-    "https://git-osx-installer.googlecode.com/files/git-1.8.4.2-intel-universal-snow-leopard.dmg"
+URLS=(
+    # MacPorts
+    "http://www.macports.org/install.php"
+
+    # Hotkeys and window management
+    "https://pqrs.org/macosx/keyremap4macbook/"
+    "https://pqrs.org/macosx/keyremap4macbook/pckeyboardhack.html.en"
+    "https://github.com/jigish/slate"
+
+    # Emacs
     "http://emacsformacosx.com/emacs-builds/Emacs-24.3-universal-10.6.8.dmg"
-    "http://iterm2.com/downloads/stable/iTerm2_v1_0_0.zip"
+
+    # iTerm2
+    "http://www.iterm2.com/#/section/home"
 
     # SSHFS
-    "http://sourceforge.net/projects/osxfuse/files/osxfuse-2.6.2/osxfuse-2.6.2.dmg/download"
-    "https://github.com/osxfuse/sshfs/releases/download/osxfuse-sshfs-2.5.0/sshfs-2.5.0.pkg"
+    "http://osxfuse.github.io/"
 
     # KeePassX
-    "http://www.keepassx.org/releases/KeePassX-0.4.3.dmg"
+    "http://www.keepassx.org/downloads"
+
+    # iStatMenus
+    "http://bjango.com/mac/istatmenus/"
 
     # Chrome
-    "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
+    "https://www.google.com/intl/en/chrome/browser/"
 )
 
 for url in "${URLS[@]}"
 do
-    echo "Downloading $url"
-    (cd ~/Downloads && curl -O $url)
+    open $url
 done
