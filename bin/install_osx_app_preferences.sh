a8c9fbc9f219326c7e35f9db61a7d51a6bec2034
Calvin Young <calvinwyoung@gmail.com>
2015-08-06 23:54:20 -0700

    Proper string escaping

--- bin/install_osx_app_preferences.sh
+++ bin/install_osx_app_preferences.sh
@@ -8,9 +8,9 @@ for filepath in $PREFERENCES_DIR/*; do
 done
 
 # Also sync Alfred preferences, which are managed separately.
-alfred_source=$HOME/Documents/.Settings/OSX__Library__Application_Support__Alfred_2/Alfred.alfredpreferences
-alfred_target=$HOME/Library/Application\ Support/Alfred\ 2/Alfred.alfredpreferences
-if [ -d $alfred_target ]; then
-    rm -rf $alfred_target
+alfred_source="$HOME/Documents/.Settings/OSX__Library__Application_Support__Alfred_2/Alfred.alfredpreferences"
+alfred_target="$HOME/Library/Application Support/Alfred 2/Alfred.alfredpreferences"
+if [ -d "$alfred_target" ]; then
+    rm -rf "$alfred_target"
 fi
-ln -sf $alfred_source $alfred_target
+ln -sf "$alfred_source" "$alfred_target"
