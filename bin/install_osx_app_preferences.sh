27a92b1705093f6af03bd4dec958347c25eb357c
Calvin Young <calvinwyoung@gmail.com>
2015-08-06 23:44:14 -0700

    Properly syncs Alfred preferences

--- bin/install_osx_app_preferences.sh
+++ bin/install_osx_app_preferences.sh
@@ -6,3 +6,6 @@ for filepath in $PREFERENCES_DIR/*; do
     echo "Installing symlink for $HOME/Library/Preferences/$(basename $filepath)"
     ln -sf $filepath $HOME/Library/Preferences/$(basename $filepath)
 done
+
+# Also sync Alfred preferences, which are managed separately.
+ln -sf $HOME/Documents/.Settings/OSX__Library__Application_Support__Alfred_2/* $HOME/Library/Application\ Support/Alfred\ 2/
