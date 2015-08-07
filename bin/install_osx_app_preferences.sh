9819c615c3b53b5fdc297f6e3be57766afedca7a
Calvin Young <calvinwyoung@gmail.com>
2015-08-06 23:55:56 -0700

    Fixes capitalization

--- bin/install_osx_app_preferences.sh
+++ bin/install_osx_app_preferences.sh
@@ -8,9 +8,10 @@ for filepath in $PREFERENCES_DIR/*; do
 done
 
 # Also sync Alfred preferences, which are managed separately.
-alfred_source="$HOME/Documents/.Settings/OSX__Library__Application_Support__Alfred_2/Alfred.alfredpreferences"
-alfred_target="$HOME/Library/Application Support/Alfred 2/Alfred.alfredpreferences"
-if [ -d "$alfred_target" ]; then
-    rm -rf "$alfred_target"
+echo "Installing Alfred preferences"
+ALFRED_SOURCE="$HOME/Documents/.Settings/OSX__Library__Application_Support__Alfred_2/Alfred.alfredpreferences"
+ALFRED_TARGET="$HOME/Library/Application Support/Alfred 2/Alfred.alfredpreferences"
+if [ -d "$ALFRED_TARGET" ]; then
+    rm -rf "$ALFRED_TARGET"
 fi
-ln -sf "$alfred_source" "$alfred_target"
+ln -sf "$ALFRED_SOURCE" "$ALFRED_TARGET"
