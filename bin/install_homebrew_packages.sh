a6e7ce7170a86aebd4a9ed29ced7c9693c2f0795
Calvin Young <calvinwyoung@gmail.com>
2016-02-17 12:03:39 -0800

    Adds unrar package from homebrew

--- bin/install_homebrew_packages.sh
+++ bin/install_homebrew_packages.sh
@@ -14,5 +14,9 @@ brew install findutils --with-default-names
 brew install grep --with-default-names
 brew install rename
 
-# Install additional applications
+# Install additional applications.
 brew install git
+brew install unrar
+
+# Need to install ispell for flyspell to work in emacs.
+brew install ispell --with-lang-en
