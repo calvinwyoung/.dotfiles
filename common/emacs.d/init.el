5e0774275a42180a970bf463bd76a2b05171a4b4
Calvin Young <calvinwyoung@gmail.com>
2019-07-06 12:34:33 -0700

    Adds package-initialize to init.el

--- common/emacs.d/init.el
+++ common/emacs.d/init.el
@@ -1,3 +1,9 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq expanded-user-emacs-directory (expand-file-name user-emacs-directory))
 
 (defvar config-dir (concat expanded-user-emacs-directory "config/")
@@ -16,3 +22,17 @@
 (load-library "lang-modes")
 (load-library "keybindings")
 (load-library "plugins")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (web-mode rjsx-mode js2-mode flycheck yasnippet yaml-mode wgrep seq org-autolist multiple-cursors lua-mode let-alist helm-projectile go-mode expand-region exec-path-from-shell deft auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
