0643ee34de4717cd1678ef91b142b8cc0e0f775f
Calvin Young <calvinwyoung@gmail.com>
2020-02-29 11:51:23 -0800

    Adds pyenv and emacs po-mode

--- common/emacs.d/config/lang-modes.el
+++ common/emacs.d/config/lang-modes.el
@@ -275,3 +275,11 @@
    sh-indent-command
    (buffer-name))
   (save-buffer))
+
+;;;;;;;;;;;
+;; PO files
+;;;;;;;;;;;
+(autoload 'po-mode "po-mode.el"
+  "Major mode for translators to edit PO files" t)
+(setq auto-mode-alist
+      (cons '("\\.po\\'\\|\\.po\\." . po-mode) auto-mode-alist))
