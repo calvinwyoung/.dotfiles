9afd48f416244187039e5a51fa942ef691ab7db0
Calvin Young <calvinwyoung@gmail.com>
2020-06-11 16:42:49 -0700

    Removes unused packages

--- darwin/slate.js
+++
@@ -1,6 +0,0 @@
-// We expect all of the actual configuration files to be a `.slate.js.d`
-// directory that's peer-level to this file.
-slate.source(".slate.js.d/settings.js");
-slate.source(".slate.js.d/helpers.js");
-slate.source(".slate.js.d/layouts.js");
-slate.source(".slate.js.d/keybindings.js");
